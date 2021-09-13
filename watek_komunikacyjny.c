489889f74febf24608c71c5fd2bb4a068ea866a9
Student inf141222 <inf141222@polluks.cs.put.poznan.pl>
2021-09-13 18:50:45 +0200

    final

--- watek_komunikacyjny.c
+++ watek_komunikacyjny.c
@@ -5,65 +5,102 @@
 void *startKomWatek(void *ptr)
 {
     MPI_Status status;
+//    int new_lamport;
     int is_message = FALSE;
+    int lowerPriority = TRUE;
     packet_t pakiet;
     /* Obrazuje pętlę odbierającą pakiety o różnych typach */
     while ( stan!=InFinish ) {
 	//debug("czekam na recv");
         MPI_Recv( &pakiet, 1, MPI_PAKIET_T, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
 
-		pthread_mutex_lock(&lamportMut);
-		if (lamport > pakiet.ts){
-			lamport++;
-		}else{
-			lamport = pakiet.ts;
-			lamport++;
-		}
-		pthread_mutex_unlock(&lamportMut);
+//		pthread_mutex_lock(&lamportMut);
+//		if (my_request > pakiet.ts){
+//			new_lamport++;
+//		}else{
+//			new_lamport = pakiet.ts;
+//			new_lamport++;
+//		}
+//		lamport = new_lamport;
+//		pthread_mutex_unlock(&lamportMut);
 
-
+	updateClock(pakiet.ts);
 		switch ( status.MPI_TAG){
 			case REQUEST:
 				//debug("Got REQUEST");
+				if(reqTime == pakiet.ts){
+					if(pakiet.src < rank){
+						lowerPriority = FALSE;
+					}else{
+						lowerPriority = TRUE;
+					}
+				}else{
+					if(reqTime < pakiet.ts){
+						lowerPriority = FALSE;
+					}else{
+						lowerPriority = TRUE;
+					}
+				}
 				switch (pakiet.data){
 					case DESK:
-						if (stan == InWaitForTables ||  stan == InWaitForTable ){
-							if (pakiet.ts < my_request) {
+						if (stan == InWaitForTables || stan == InWaitForTable ){
+					//		debug("BIURKA: moj zegar %i, zegar przych %i", pakiet.ts, reqTime);
+							if (lowerPriority) {
 								pakiet.tables = team_size;
 								sendPacket(&pakiet, pakiet.src, ACK);
+								updateClock(my_request);
+								//my_request = new_lamport;
 							}else{
+			//					debug("Bede czekal na odp desk");
 								table[pakiet.src] = 1;
 							} 
 						}else if(stan == InTables || stan == InTable){
+			//				debug("Bede czekal na odp DESK od %i", pakiet.src);
 							table[pakiet.src] = 1;
 						}else {
 							pakiet.tables = team_size;
 							sendPacket(&pakiet, pakiet.src, ACK);
+							updateClock(my_request);
+							//my_request = new_lamport;
 						}
 					break;
 					case ROOM:
 						if (stan == InWaitForRoom ){
-							if (pakiet.ts < my_request) {
+                                          //              debug("SALKI: moj zegar %i, zegar przych %i", pakiet.ts, reqTime);
+							if (lowerPriority){
+								updateClock(my_request);
+								//my_request = new_lamport;
 								sendPacket(&pakiet, pakiet.src, ACK);
 							}else{
+							//	debug("Bede czekal na odp ROOM od %i", pakiet.src);
 								room[pakiet.src] = 1;
 							} 
 						}else if(stan == InRoom){
 							room[pakiet.src] = 1;
-						}else {
+							//debug("Bede czekal na odp ROOM od %i", pakiet.src);
+						}else {                      
+		      					updateClock(my_request);
+                                                        //my_request = lamport;
 							sendPacket(&pakiet, pakiet.src, ACK);
 						}
 					break;
 					case LAUNCHPAD:
 						if (stan == InWaitForLaunchPad){
-							if (pakiet.ts < my_request) {
+                                            //            debug("PAS: moj zegar %i, zegar przych %i", pakiet.ts, reqTime);
+							if (lowerPriority) {
+								updateClock(my_request);
+								//my_request = new_lamport;
 								sendPacket(&pakiet, pakiet.src, ACK);
 							}else{
+							//	debug("Bede czekal na odp LAUNCH od %i", pakiet.src);
 								launchpad[pakiet.src] = 1;
 							} 
 						}else if(stan == InLaunchPad){
+							//debug("Bede czekal na odp LAUNCH od %i", pakiet.src);
 							launchpad[pakiet.src] = 1;
 						}else {
+		                                        updateClock(my_request);
+                                                        //my_request = lamport;
 							sendPacket(&pakiet, pakiet.src, ACK);
 						}
 					break;
@@ -76,39 +113,46 @@ void *startKomWatek(void *ptr)
 						if (stan == InWaitForTables || stan == InWaitForTable){
 							pthread_mutex_lock( &teamMut );
 							current_tables_taken = current_tables_taken - pakiet.tables;
-							pthread_mutex_unlock( &teamMut );
-							table[pakiet.src] = 0;
+						//	table[pakiet.src] = 0;
 							debug("Czekam aż będzie dostępne więcej niż %i a jest %i", total_desks, current_tables_taken);
 							if (current_tables_taken <= total_desks){
 								if (stan == InWaitForTables){
+									debug("Zajmuję biurka");
 									changeState(InTables);
 								}else{
+									debug("Zajmuję biurko");
 									changeState(InTable);
 								}
 							}
+                                                        pthread_mutex_unlock( &teamMut );
 						}
+						
 					break;
 					case ROOM:
 						if (stan == InWaitForRoom){
 							pthread_mutex_lock( &teamMut );
 							current_rooms_taken--;
-							pthread_mutex_unlock( &teamMut );
-							room[pakiet.src] = 0;
+						//	room[pakiet.src] = 0;
+							debug("Zajetych pokojow %i, a moze byc %i",current_rooms_taken, total_rooms);
 							if (current_rooms_taken <= total_rooms){
+								//room[pakiet.src] = 0;
+								debug("Zajmuję pokój");
 								changeState(InRoom);
 							}
+							pthread_mutex_unlock( &teamMut );
 						}
 					break;
 					case LAUNCHPAD:
 						if (stan == InWaitForLaunchPad){
-							debug("Zajętych jest %i a może być %i", current_pads_taken, total_pads);
+							debug("Zajętych jest %i a może być %i", current_pads_taken-1, total_pads);
 							pthread_mutex_lock( &teamMut );
 							current_pads_taken--;
-							pthread_mutex_unlock( &teamMut );
-							launchpad[pakiet.src] = 0;
+						//	launchpad[pakiet.src] = 0;
 							if (current_pads_taken <= total_pads){
+								debug("Zajmuję pole startowe");
 								changeState(InLaunchPad);
 							}
+                                                        pthread_mutex_unlock( &teamMut );
 						}
 					break;
 				}
@@ -118,7 +162,6 @@ void *startKomWatek(void *ptr)
 				increaseTeamSize(pakiet.data);
 			break;
 		}
-
         /*switch ( status.MPI_TAG ) {
 	    case FINISH: 
                 changeState(InFinish);
